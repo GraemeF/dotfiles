3c3e7405814ff0daf4bc8d4dad8e4f8525352987
Graeme Foster <graemef@gmail.com>
2013-09-29 18:36:21 +0100

    Added Redl.vim to leiningen.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,8 @@
-{ :user { :plugins [[lein-pprint "1.1.1"]
-                    [lein-ancient "0.4.4"]
-                    [lein-autoexpect "1.0"]
-                    [lein-midje "3.1.0"]] }}
+{:user {:dependencies [[spyscope "0.1.3"]
+                       [redl "0.1.0"]]
+        :injections [(require 'spyscope.core)
+                     (require '[redl complete core])]
+        :plugins [[lein-pprint "1.1.1"]
+                  [lein-ancient "0.4.4"]
+                  [lein-autoexpect "1.0"]
+                  [lein-midje "3.1.0"]] }}
